8e8de9bfb3f5c6f4e0eecba7371115e9a43297e9
Jamilly Cuco <jamillysantos2507@gmail.com>
2020-10-16 15:56:29 -0300

    last commit day 4

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState} from 'react';
 import MapView, {Marker, Callout,  PROVIDER_GOOGLE} from 'react-native-maps';
 import {Feather} from '@expo/vector-icons';
 import { StyleSheet, Text, View, Dimensions} from 'react-native';
 import mapMarker from '../images/map-marker.png';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler'
 import api from '../services/api';
 
@@ -18,11 +18,11 @@ export default function OrphanagesMap(){
     const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
     const navigation = useNavigation();
 
-    useEffect(() => {
+    useFocusEffect(() => {
       api.get('orphanages').then(response => {
         setOrphanages(response.data);
       });
-    }, []);
+    });
 
     function handleNavigationToOrphanageDetails(id : number){
         navigation.navigate('OrphanageDetails', { id});
